5b338f8413f26c1d701f020a2469076452258c7e
Sulecs1 <sule.akcay3@ogr.sakarya.edu.tr>
2021-02-24 02:20:26 +0300

    değişiklikler yapıldı

--- diabetesset.py
+++ diabetesset.py
@@ -38,6 +38,9 @@ def data_understand(df):
     print("------------------------------------------------------------------------")
     print(df.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
     print("------------------------------------------------------------------------")
+    # Isı haritasında, daha parlak renkler daha fazla korelasyonu gösterir.
+    # Tablodan ve ısı haritasından da görebileceğimiz gibi, glikoz seviyeleri, yaş, vücut kitle indeksi ve gebelik sayısı, sonuç değişkeni ile önemli bir korelasyona sahiptir. Ayrıca yaş ve gebelikler veya insülin ve cilt kalınlığı gibi özellik çiftleri arasındaki korelasyona dikkat ediniz.
+
     corr = df.corr()
     print(corr)
     print("------------------------------------------------------------------------")
@@ -46,7 +49,12 @@ def data_understand(df):
                 yticklabels=corr.columns)
     plt.show()
     print("------------------------------------------------------------------------")
-
+    print(sns.pairplot(df));
+    plt.show()
+    print("------------------------------------------------------------------------")
+    df.hist(bins=20, color="#1c0f45", edgecolor='orange', figsize=(15, 15));
+    plt.show()
+    print("------------------------------------------------------------------------")
 
 data_understand(df)
 
@@ -82,33 +90,37 @@ def missing_values_table(df):
         print("\nThere are {} columns with missing values\n".format(len(missing_df)))
 
 missing_values_table(df)
+#################################
+# DATA PREPROCESSING            #
+#################################
+
+df_new = df.copy()
+#aşağıdaki sıfır değerlerini NAN değerleri işe değiştirdik
+df_new = df_new.loc[:, "Pregnancies": "Age"].replace(0, np.nan).dropna()
+
+#döngü ile her değişkeni gezdik burada değişkenkenlerdeki diyabet olup olmama oranına göre ortalamasını aldık
+for col in df_new.columns:
+    if col !="Outcome":
+        print(col.upper(), "\n", "DİYABET HASTASI:", df_new.loc[df["Outcome"] == 1, col].median(), "\n",
+              "ŞEKER HASTASI DEĞİL:", df_new.loc[df["Outcome"] == 0, col].median())
+
+#Sutunlardaki sıfır değerine kırılıma ataması
 
 
+def col_nan_assigment(df):
+    for x in df.columns:
+        for y in range(len(df)):
+            if x != "Outcome":
+                if df.loc[y, x] == 0:
+                    if df.loc[y, "Outcome"] == 1:
+                        df.loc[y, x] = df.loc[df["Outcome"] == 1, x].median()
 
+                    else:
+                        df.loc[y, x] = df.loc[df["Outcome"] == 0, x].median()
 
+col_nan_assigment(df)
+df.head()
 
-
-
-
-
-
-
-
-
-df.isnull().sum()
-#ısı haritası kullanarak değişkenlerin birbirlerine göre korelasyon durumları incelendi
-corr = df.corr()
-print(corr)
-
-#Isı haritasında, daha parlak renkler daha fazla korelasyonu gösterir.
-# Tablodan ve ısı haritasından da görebileceğimiz gibi, glikoz seviyeleri, yaş, vücut kitle indeksi ve gebelik sayısı, sonuç değişkeni ile önemli bir korelasyona sahiptir. Ayrıca yaş ve gebelikler veya insülin ve cilt kalınlığı gibi özellik çiftleri arasındaki korelasyona dikkat ediniz.
-sns.heatmap(corr,
-         xticklabels=corr.columns,
-         yticklabels=corr.columns)
-plt.show()
-
-df.hist(bins=20, color="#1c0f45", edgecolor='orange', figsize = (15,15));
-plt.show()
 
 #kaç kişinin şeker hastası olduğuna ve kaçının şeker hastası olmadığını bulmak için boxplot grafiğinden yararlanacağım
 sns.countplot(x="Outcome", data=df)
@@ -148,3 +160,23 @@ df.groupby(["NEW_AGE_CAT", "Outcome"]).size().unstack(fill_value=0).apply(lambda
 #sonuçlara bakılırsa gençlerin hasta olmama oranı daha azdır
 
 df.groupby(["NEW_AGE_CAT", "Outcome"]).mean()
+
+#####################
+# ONE-HOT ENCODING  #
+#####################
+
+#Aykırı değerler
+num_cols = [col for col in df.columns if len(df[col].unique()) > 18 and df[col].dtypes != "O"]
+df.shape
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+ohe_cols = [col for col in df.columns if 10 >= len(df[col].unique()) > 2]
+
+one_hot_encoder(df, ohe_cols).head()
+
+one_hot_encoder(df, ohe_cols, drop_first=True).head()
